13adc6d399968b2050255ed753737ae5d304a30b
Christian Pulmano <cpulmano@ateneo.edu>
2022-06-16 10:45:39 +0800

    Upload latest file

--- converttogzip.py
+++ converttogzip.py
@@ -1,7 +1,8 @@
 import pandas as pd
-
+import os
 
 if __name__ == "__main__":
-	df = pd.read_csv("20220325_CAR.csv",dtype=str)
-
+	latest = [x for x in os.listdir() if x.endswith(".csv")][0]
+	df = pd.read_csv(latest,dtype=str)
 	df.to_parquet('df.linelist.140000000.gzip',compression='gzip')
+	print("Done")
